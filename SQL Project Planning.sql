67416503c2ab9c25ddf11b9cf9a5332d1fc08cb6
Sailash <38294590+Sailash@users.noreply.github.com>
2021-08-20 15:51:12 +0530

    version 01

--- SQL Project Planning.sql
+++ SQL Project Planning.sql
@@ -1,6 +1,6 @@
-SELECT Start_Date, MIN(End_Date) 
-FROM (SELECT Start_Date FROM Projects WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)) as a, 
-(SELECT end_date FROM PROJECTS WHERE end_date NOT IN (SELECT start_date FROM PROJECTS)) as b 
+SELECT start_date, MIN(end_date) 
+FROM (SELECT start_date FROM projects WHERE start_date NOT IN (SELECT end_date FROM projects)) as a, 
+     (SELECT end_date FROM projects WHERE end_date NOT IN (SELECT start_date FROM projects)) as b 
 where start_date < end_date 
 GROUP BY start_date 
 ORDER BY datediff(start_date, MIN(end_date)) DESC, start_date
